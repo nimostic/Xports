4d309e832e153c4bc441ee1238b77f6c0c50074c
Abu Nayeem Riyad <rionimo5@gmail.com>
2025-12-24 22:02:45 +0600

    payment implementation complete

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from "react";
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://xports-server.vercel.app",
 });
 const useAxiosSecure = () => {
   return axiosSecure;
